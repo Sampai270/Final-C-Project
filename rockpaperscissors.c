b0efe36a1396986212addd087489e85fd68114e1
Samuel Hui <samuelhui08@gmail.com>
2020-12-09 23:09:14 -0500

    added funny language

--- rockpaperscissors.c
+++ rockpaperscissors.c
@@ -19,41 +19,41 @@ int main()
             		int computer=rand()%3+1;
             	if(choice==1){
                 	if(computer==1){
-                    	printf("Draw\n");
+                    	printf("The Computer chose rock, too!\n Draw!\n");
                 	}
                 if(computer==2){
-                    printf("Computer Wins!\n");
+                    printf("The Computer chose paper!\n The Computer wins!\n");
                     Cscore= Cscore + 1;
                 }
                 if(computer==3){
-                    printf("Player Wins\n");
+                    printf("The Computer chose scissors!\n You win!\n");
                     Pscore = Pscore + 1;
                 }
             }
               else  if(choice==2){
                     if(computer==2){
-                        printf("Draw\n");
+                        printf("The Computer chose paper, too!\n Draw!\n");
                     }
                     if(computer==3){
-                        printf("Player Wins!\n");
-                        Pscore = Pscore + 1;
+                        printf("The Computer chose scissors!\n The Computer wins!\n");
+                        Cscore = Cscore + 1;
                     }
                     if(computer==1){
-                        printf("Computer Wins!\n");
-                        Cscore= Cscore + 1;
+                        printf("The Computer chose rock!\n You win!\n");
+                        Pscore= Pscore + 1;
                     }
                 }
                 else if(choice==3){
                     if(computer==3){
-                        printf("Draw\n");
+                        printf("The Computer chose scissors, too!\n Draw!\n");
                     }
                     if(computer==2){
-                        printf("Computer Wins!\n");
-                        Cscore= Cscore + 1;
+                        printf("The Computer chose paper!\n You win!\n");
+                        Pscore= Pscore + 1;
                     }
                     if(computer==1){
-                        printf("Player Wins!\n");
-                        Pscore = Pscore + 1;
+                        printf("The Computer chose rock!\n The Computer wins!\n");
+                        Cscore = Cscore + 1;
 
                     }
 		}
@@ -64,11 +64,11 @@ int main()
 
 	}         
 	else if(Cscore == 2 ){
-                printf("Computer wins %d to %d\n",Cscore,Pscore);
+                printf("Better luck next time! The Computer wins %d to %d\n",Cscore,Pscore);
 		break;
             }
             else if(Pscore == 2 ){
-                printf("Player wins %d to %d\n",Pscore,Cscore);
+                printf("Great Job! You win %d to %d\n",Pscore,Cscore);
 		break;
             }
 	}
