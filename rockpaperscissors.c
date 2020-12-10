9ec9591a4f4a07de927d11e5d8e6da6b19335043
Samuel Hui <samuelhui08@gmail.com>
2020-12-10 17:17:41 -0500

    added comments to various lines of code

--- rockpaperscissors.c
+++ rockpaperscissors.c
@@ -6,23 +6,23 @@ int main()
     /*int R=1;
     int P=2;
     int S=3;*/
-    int Pscore =0;
-    int Cscore =0;
-    int choice;
-    printf("Rock=1 , Paper= 2, and Scissors= 3\n");
+    int Pscore =0; /*Keeps track how many games the player has won, which will start 0*/
+    int Cscore =0; /*Keeps track how many games the computer has won, which will also start 0*/
+    int choice; /*defines the input of the player*/
+    printf("Rock=1 , Paper= 2, and Scissors= 3\n"); /*This lets the player know what number corresponds to which action*/
 
-    while (Cscore < 2 && Pscore < 2){
-            printf("Enter your choice:");
-            scanf("%d",&choice);
-            int computer=rand()%3+1;
-            switch (choice) {
-                case 1:
-                    switch (computer) {
-                        case 1:
-                            printf("The Computer chose rock, too!\nDraw!\n");
-                            break;
-                        case 2:
-                            printf("The Computer chose paper!\nThe Computer wins!\n");
+    while (Cscore < 2 && Pscore < 2){ /*The game will continue unless either the player or the computer has won 2 games*/
+            printf("Enter your choice:"); /*Asks user to input a choice*/
+            scanf("%d",&choice); /*Game processes the player's choice*/
+            int computer=rand()%3+1; /*The computer will know randomly pick an number between 1 and 3*/
+            switch (choice) { /*Either one of four scenarios will happen depending on the number you chose*/
+                case 1: /*If you picked 1*/
+                    switch (computer) { /*Either one of three scenarios will happen depending on the number the computer chose*/
+                        case 1: /*If the computer picked 1*/
+                            printf("The Computer chose rock, too!\nDraw!\n"); /*Lets the player know the computer has chosen rock, meaning it's a draw*/
+                            break; /*Breaks the program out of the case*/
+                        case 2: /*If the computer picked 2*/
+                            printf("The Computer chose paper!\nThe Computer wins!\n"); /*Lets the player know the computer has chosen paper, meaning the player lost*/
                             Cscore= Cscore + 1;
 			    break;
                     	case 3:
