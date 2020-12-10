81473b69ae358eed2aff323090441a3d2fd8ac76
Sampai270 <70983801+Sampai270@users.noreply.github.com>
2020-12-10 10:03:40 -0500

    edited formatting

--- rockpaperscissors.c
+++ rockpaperscissors.c
@@ -12,65 +12,62 @@ int main()
     int choice;
     printf("Rock=1 , Paper= 2, and Scissors= 3\n");
 
-	for (i=0; i<10; i++){
-		if (Cscore < 2 && Pscore < 2){
-        	printf("Enter your choice:");
-        	scanf("%d",&choice);
-            		int computer=rand()%3+1;
-            	if(choice==1){
-                	if(computer==1){
-                    	printf("The Computer chose rock, too!\n Draw!\n");
-                	}
-                if(computer==2){
+    for (i=0; i<10; i++){
+        if (Cscore < 2 && Pscore < 2){
+            printf("Enter your choice:");
+            scanf("%d",&choice);
+            int computer=rand()%3+1;
+            if (choice==1) {
+                if (computer==1) {
+                    printf("The Computer chose rock, too!\n Draw!\n");
+                }
+                if (computer==2) {
                     printf("The Computer chose paper!\n The Computer wins!\n");
                     Cscore= Cscore + 1;
                 }
-                if(computer==3){
+                if (computer==3) {
                     printf("The Computer chose scissors!\n You win!\n");
                     Pscore = Pscore + 1;
                 }
             }
-              else  if(choice==2){
-                    if(computer==2){
-                        printf("The Computer chose paper, too!\n Draw!\n");
-                    }
-                    if(computer==3){
-                        printf("The Computer chose scissors!\n The Computer wins!\n");
-                        Cscore = Cscore + 1;
-                    }
-                    if(computer==1){
-                        printf("The Computer chose rock!\n You win!\n");
-                        Pscore= Pscore + 1;
-                    }
+            else if (choice==2) {
+                if (computer==2) {
+                    printf("The Computer chose paper, too!\n Draw!\n");
                 }
-                else if(choice==3){
-                    if(computer==3){
-                        printf("The Computer chose scissors, too!\n Draw!\n");
-                    }
-                    if(computer==2){
-                        printf("The Computer chose paper!\n You win!\n");
-                        Pscore= Pscore + 1;
-                    }
-                    if(computer==1){
-                        printf("The Computer chose rock!\n The Computer wins!\n");
-                        Cscore = Cscore + 1;
+                if (computer==3) {
+                    printf("The Computer chose scissors!\n The Computer wins!\n");
+                    Cscore = Cscore + 1;
+                }
+                if (computer==1) {
+                    printf("The Computer chose rock!\n You win!\n");
+                    Pscore= Pscore + 1;
+                }
+            } else if (choice==3) {
+                if (computer==3) {
+                    printf("The Computer chose scissors, too!\n Draw!\n");
+                }
+                if (computer==2) {
+                    printf("The Computer chose paper!\n You win!\n");
+                    Pscore= Pscore + 1;
+                }
+                if (computer==1) {
+                    printf("The Computer chose rock!\n The Computer wins!\n");
+                    Cscore = Cscore + 1;
+                }
+            }
+            else {
+                printf("Wrong Answer\n");
+            }
 
-                    }
-		}
-                
-       else{
-            printf("Wrong Answer\n");
+        }        
+        else if (Cscore == 2 ) {
+            printf("Better luck next time! The Computer wins %d to %d\n",Cscore,Pscore);
+            break;
         }
-
-	}         
-	else if(Cscore == 2 ){
-                printf("Better luck next time! The Computer wins %d to %d\n",Cscore,Pscore);
-		break;
-            }
-            else if(Pscore == 2 ){
-                printf("Great Job! You win %d to %d\n",Pscore,Cscore);
-		break;
-            }
-	}
-   return(0);
+        else if (Pscore == 2 ) {
+            printf("Great Job! You win %d to %d\n", Pscore, Cscore);
+            break;
+        }
+    }
+    return(0);
 }
